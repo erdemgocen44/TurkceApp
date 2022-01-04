43e47be5fd49e55fd2be1ba1969a7979c35cee32
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-01-05 00:06:01 +0100

    Divider bitti

--- lib/core/widget/listTileCustom.dart
+++ lib/core/widget/listTileCustom.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
 ListTile CustomListTile(BuildContext context, Icon ikon, String yazi,
-    FontWeight kalinlik, double size, Color renk) {
-  return ListTile(
-    leading: ikon,
-    title: Text(
-      yazi,
-      style: TextStyle(fontWeight: kalinlik, fontSize: size, color: renk),
-    ),
-    onTap: () {
-      Navigator.pop(context);
-    },
-  );
-}
+        FontWeight kalinlik, Color renk) =>
+    ListTile(
+      leading: ikon,
+      title: Text(
+        yazi,
+        style: TextStyle(fontWeight: kalinlik, fontSize: 16, color: renk),
+      ),
+      onTap: () {
+        Navigator.pop(context);
+      },
+    );
--- lib/pages/yanmenu.dart
+++ lib/pages/yanmenu.dart
@@ -21,7 +21,7 @@ class YanMenu extends StatelessWidget {
               child: Image.asset(
                 "assets/images/flower.png",
                 fit: BoxFit.fill,
-                width: MediaQuery.of(context).size.width * 0.35,
+                width: MediaQuery.of(context).size.width * 0.25,
               ),
             ),
             Expanded(
@@ -29,19 +29,140 @@ class YanMenu extends StatelessWidget {
                 children: [
                   CustomListTile(
                       context,
-                      Icon(
+                      const Icon(
                         Icons.adb_sharp,
-                        color: Color(0xed923456),
+                        size: 35,
+                        color: Color(0xFF9ec22a),
                       ),
                       "Ortaokul 5",
                       FontWeight.bold,
-                      20,
                       Colors.pink),
+                  dividercustom(),
+                  CustomListTile(
+                      context,
+                      const Icon(
+                        Icons.face_sharp,
+                        size: 35,
+                        color: Color(0xFF9ec22a),
+                      ),
+                      "Ortaokul 6",
+                      FontWeight.bold,
+                      Colors.pink),
+                  dividercustom(),
+                  CustomListTile(
+                      context,
+                      const Icon(
+                        Icons.mode_edit_sharp,
+                        size: 35,
+                        color: Color(0xFF9ec22a),
+                      ),
+                      "Ortaokul 7",
+                      FontWeight.bold,
+                      Colors.pink),
+                  dividercustom(),
+                  CustomListTile(
+                      context,
+                      const Icon(
+                        Icons.light_sharp,
+                        size: 35,
+                        color: Color(0xFF9ec22a),
+                      ),
+                      "Ortaokul 8",
+                      FontWeight.bold,
+                      Colors.pink),
+                  dividercustom(),
+                  ExpansionTile(
+                    leading: const Icon(
+                      Icons.phone_android,
+                      color: Colors.blueAccent,
+                    ),
+                    title: defaulttext(
+                        "Video Dersler", 20, FontWeight.bold, Colors.black),
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(left: 10),
+                        child: CustomListTile(
+                            context,
+                            const Icon(
+                              Icons.supervised_user_circle,
+                              color: Colors.orangeAccent,
+                            ),
+                            "5.Sınıf",
+                            FontWeight.w500,
+                            Colors.blueGrey),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 10),
+                        child: CustomListTile(
+                            context,
+                            const Icon(
+                              Icons.supervised_user_circle,
+                              color: Colors.orangeAccent,
+                            ),
+                            "6.Sınıf",
+                            FontWeight.w500,
+                            Colors.greenAccent),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 10),
+                        child: CustomListTile(
+                            context,
+                            const Icon(
+                              Icons.supervised_user_circle,
+                              color: Colors.lightBlueAccent,
+                            ),
+                            "7.Sınıf",
+                            FontWeight.w500,
+                            Colors.blueGrey),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 10),
+                        child: CustomListTile(
+                            context,
+                            const Icon(
+                              Icons.supervised_user_circle,
+                              color: Colors.lightGreenAccent,
+                            ),
+                            "8.Sınıf",
+                            FontWeight.w500,
+                            Colors.blueGrey),
+                      ),
+                    ],
+                  ),
+                  dividercustom(),
+                  CustomListTile(
+                      context,
+                      const Icon(
+                        Icons.family_restroom_sharp,
+                        color: Colors.lightGreenAccent,
+                      ),
+                      "HAKKIMIZDA",
+                      FontWeight.w500,
+                      Colors.blueGrey),
+                  CustomListTile(
+                      context,
+                      const Icon(
+                        Icons.contact_phone,
+                        color: Colors.lightGreenAccent,
+                      ),
+                      "İLETİŞİM",
+                      FontWeight.w500,
+                      Colors.blueGrey),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Padding dividercustom() {
+    return const Padding(
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: Divider(
+        height: 2,
+        color: Colors.blueGrey,
       ),
     );
   }

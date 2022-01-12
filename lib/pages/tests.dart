798981fe689f4eebe3ff5097a86357b237f47e4c
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-01-12 20:05:02 +0100

    http kutuphanesi eklendi

--- lib/pages/tests.dart
+++ lib/pages/tests.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class TestPage extends StatefulWidget {
   const TestPage({Key? key}) : super(key: key);
@@ -8,7 +9,15 @@ class TestPage extends StatefulWidget {
 }
 
 class _TestPageState extends State<TestPage> {
-  final String _htmlData = " ";
+  String _htmlData = " ";
+
+  Future _istek() async {
+    await http
+        .get(Uri.parse(
+            'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjlg7-J26z1AhWrgv0HHeWYAlEQ3e4CegQIBBAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dsm5amZjPUWA&usg=AOvVaw0Ln-oJfOhp_IVe7nF6fj_k'))
+        .then((gelenVeri) => _htmlData = gelenVeri.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

3ee7b986ac6480212df01cec8abe3157f41c03e0
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-01-18 18:50:29 +0100

    do

--- lib/core/widget/divider_custom.dart
+++ lib/core/widget/divider_custom.dart
@@ -4,7 +4,7 @@ Padding dividercustom() {
   return const Padding(
     padding: EdgeInsets.only(left: 10, right: 10),
     child: Divider(
-      height: 6,
+      height: 5,
       color: Color(0xFF9ec22a),
     ),
   );

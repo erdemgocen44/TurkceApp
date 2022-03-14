9e7fefaf868645d0b089bac72a0e8e12f5e9a122
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-14 22:16:34 +0100

    ana sayfada hata hala var

--- lib/pages/homepage/core/functions_widget.dart
+++ lib/pages/homepage/core/functions_widget.dart
@@ -1,10 +1,27 @@
 import 'package:aba_app/pages/homepage/core/models.dart';
+import 'package:http/http.dart' as http;
 
 abstract class Islemler {
   late List<SlaytModel> slaytlar;
+  Future<bool> istek();
+  late String veriler;
 }
 
 class IslemDetay implements Islemler {
   @override
   List<SlaytModel> slaytlar = [];
+
+  @override
+  Future<bool> istek() async {
+    await http.get(Uri.parse("https://abilar.org/")).then((gelenVeri) {
+      veriler = gelenVeri.body
+          .replaceAll("\n", "")
+          .replaceAll("\t", "")
+          .replaceAll("  ", "");
+    });
+    return true;
+  }
+
+  @override
+  late String veriler;
 }
